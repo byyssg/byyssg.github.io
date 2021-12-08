2f3a0cefc6384c58c77c94caedcc23a6658a2197
byyssg <604100426@qq.com>
2021-12-08 16:27:07 +0800

    Site updated: 2021-12-08 16:27:07

--- js/snow.js
+++ js/snow.js
@@ -1,3 +1,5 @@
+<script type="text/javascript" src="http://libs.baidu.com/jquery/1.8.3/jquery.js"></script>
+<script type="text/javascript" src="http://libs.baidu.com/jquery/1.8.3/jquery.min.js"></script>
 /*样式一*/
 (function($){
 	$.fn.snow = function(options){
